7a6ce60f1a04124277dd29290f748bc7e4a74fd0
henio <jacek.styrylski@gmail.com>
2018-03-21 18:44:39 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mongo_for_dot_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
